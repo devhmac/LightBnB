9bab5cf53cf866e86b6c5c790a136a451ed05a7f
Devin MacGillivray <devhmac@gmail.com>
2021-03-31 20:04:56 +0000

    changes to reservations

--- seeds/01_seeds.sql
+++ seeds/01_seeds.sql
@@ -13,6 +13,6 @@ VALUES (1,'speedy house','description','imgfile','imgfile2', $120,9,2,3,'Canada'
 
 -- reservation seed data
 INSERT INTO reservations (guest_id, property_id, start_date, end_date) 
-VALUES (1, 1, '2018-09-11', '2018-09-26'),
-(2, 2, '2019-01-04', '2019-02-01'),
-(3, 3, '2021-10-01', '2021-10-14');
+VALUES (1, 2, '2018-09-11', '2018-09-26'),
+(2, 3, '2019-01-04', '2019-02-01'),
+(3, 1, '2021-10-01', '2021-10-14');

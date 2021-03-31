02ff4ad79a56e3e422de0567de52af1a3323bed0
Devin MacGillivray <devhmac@gmail.com>
2021-03-31 19:11:45 +0000

    properties table created

--- migrations/01_schema.sql
+++ migrations/01_schema.sql
@@ -1,6 +1,30 @@
+DROP TABLE IF EXISTS
+DROP TABLE IF EXISTS
+DROP TABLE IF EXISTS
+DROP TABLE IF EXISTS
+
+
 CREATE TABLE users (
   id SERIAL PRIMARY KEY,
   name VARCHAR(255) NOT NULL,
   email VARCHAR(255) NOT NULL,
   password VARCHAR(255) NOT NULL
-);
+)
+CREATE TABLE properties (
+  id SERIAL PRIMARY KEY,
+  owner_id INTEGER NOT NULL REFERENCES users(id) ON DELETE CASCADE,
+  title VARCHAR(255) NOT NULL,
+  description TEXT,
+  thumbnail_photo_url VARCHAR(255),
+  cover_photo_url VARCHAR(255),
+  cost_per_night INTEGER,
+  parking_spaces INTEGER,
+  number_of_bathrooms INTEGER,
+  number_of_bedrooms INTEGER,
+  country VARCHAR(100) NOT NULL,
+  street VARCHAR(255) NOT NULL,
+  city VARCHAR(255) NOT NULL,
+  PROVINCE VARCHAR(255) NOT NULL,
+  post_code VARCHAR(50) NOT NULL,
+  ACTIVE BOOLEAN
+)
